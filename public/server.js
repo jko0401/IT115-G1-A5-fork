db6a9a23b510de989b4fd1cc419af87670378835
jko0401 <jko0401@gmail.com>
2020-11-30 17:56:59 -0800

    moved path

--- public/server.js
+++ public/server.js
@@ -2,8 +2,9 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
+app.use(express.static('public'))
 app.get('/', (req, res) => {
-    res.sendFile('./index.html', { root: __dirname });
+    res.sendFile('./public/index.html', { root: __dirname });
 });
 
 app.listen(port, () => console.log(`listening on port ${port}!`));
