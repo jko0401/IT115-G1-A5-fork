fd58241fc8190433cc84f325ab0c1d32650e5e65
jko0401 <jko0401@gmail.com>
2020-11-30 18:23:30 -0800

    testing

--- public/server.js
+++ public/server.js
@@ -2,14 +2,14 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.use(express.static('public'))
+app.use(express.static('public'));
 
-app.get('/', (req, res) => {
+/*app.get('/', (req, res) => {
    res.sendFile('/index.html', { root: __dirname });
 });
 
 app.get('/public/script.js', function(req, res) {
     res.sendFile(path.join(__dirname + '/script.js'));
 });
-
+*/
 app.listen(port, () => console.log(`listening on port ${port}!`));
